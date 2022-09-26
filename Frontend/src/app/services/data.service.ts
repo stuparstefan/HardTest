71240803450df0913b29e879b1bc9db89439575f
Stefan Stupar <stupar.git@gmail.com>
2022-09-26 22:50:22 +0200

    2.2 Tax FE

--- Frontend/src/app/services/data.service.ts
+++ Frontend/src/app/services/data.service.ts
@@ -11,9 +11,10 @@ export class DataService {
 
   constructor(private http: HttpClient) {}
 
-  public uploadFile(file: any): Observable<any> {
+  public uploadFile(file: any, taxPercentage: number): Observable<any> {
     const formData = new FormData();
     formData.append('file', file, file.name);
+    formData.append('taxPercentage', taxPercentage.toString());
     return this.http.post(`${this.baseUrl}/data`, formData);
   }
 }
